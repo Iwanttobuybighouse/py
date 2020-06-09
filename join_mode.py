32c83f9c58846d099ad8bf6ebde33987a539a07c
Your Name <16yydeng2@stu.edu.cn>
2020-06-09 21:53:40 +0800

    add join

---
+++ join_mode.py
@@ -0,0 +1,10 @@
+new_str = '-'.join('abc')
+print(new_str)
+
+#列表--list=['a','v','o','9']--数字
+list1=['a','v','o','9']
+result = ' '.join(list1)
+print(result)
+
+result = ''.join(list1)
+print(type(result))
